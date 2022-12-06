486edcad60a2445c1045cd7457349cab8722be0b
tutankatyson <61758544+tutankatyson@users.noreply.github.com>
2022-12-07 00:43:58 +0100

    Update tools.h

--- tools.h
+++ tools.h
@@ -15,9 +15,20 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+
+
 
 void printbuff(char *str, char *name)
 
